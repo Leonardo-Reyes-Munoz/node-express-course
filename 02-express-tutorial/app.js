fa301e2497817af16491432003043dcbc80e24b4
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-10-11 19:55:08 -0700

    tutorial

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,18 @@
-console.log('Express Tutorial')
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// setup static and middleware
+app.use(express.static("./navbar-app"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// });
+
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000. Woot Woot!");
+});
