c8f6c71fa0fea7bbb8eefac6a00344e6a6fd1ca7
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-10-13 13:22:48 -0700

    import express and set up use, all, and listen

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,12 @@
-console.log('Express Tutorial')
+const express = require("express");
+const app = express();
+
+app.use(express.static("./public"));
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1> Oh no! Your page was not found.</h1>");
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on port 3000. Woot woot!");
+});
