85cc14d50de26facfd6dee9468117c4d5f68a0a5
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-10-13 16:11:56 -0700

    update tutoril

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,37 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const { products } = require("./data");
 
-// setup static and middleware
-app.use(express.static("./navbar-app"));
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+});
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// });
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  console.log(req.params);
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send(`Product "${productID}" Does not Exist`);
+  }
+  res.json(singleProduct);
+});
+
+app.get("/api/products/:productsID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("Hello test");
 });
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000. Woot Woot!");
+  console.log("Server is listening on PORT 3000! What what!");
 });
