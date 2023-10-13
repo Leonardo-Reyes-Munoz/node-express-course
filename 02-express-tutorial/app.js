c2a391aa3e684fb357514bd6a22b98a6235a9d16
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-10-13 16:28:51 -0700

    add products route with params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
+const { products } = require("./data");
 
 app.use(express.static("./public"));
+
+app.get("/api/v1/test", (req, res) => {
+  res.json({ message: "It worked" });
+});
+
+app.get("/api/v1/products", (req, res) => {
+  res.send(products);
+});
+
+app.get("/api/v1/products/:productID", (req, res) => {
+  const idToFind = parseInt(req.params.productID);
+  const product = products.find((p) => p.id === idToFind);
+  if (!product) {
+    return res.status(404).json({ message: "That product was not found" });
+  }
+  res.json(product);
+});
 
 app.all("*", (req, res) => {
   res.status(404).send("<h1> Oh no! Your page was not found.</h1>");
