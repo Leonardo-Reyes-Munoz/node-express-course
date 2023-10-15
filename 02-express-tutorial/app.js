ae6992b6889c310a3997acc54e40a7f7b3a4e099
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-10-15 09:24:44 -0700

    add week 3 lesson

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -21,6 +21,26 @@ app.get("/api/v1/products/:productID", (req, res) => {
   res.json(product);
 });
 
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query);
+  let newProducts = [...products];
+  const { search, limit, priceLimit } = req.query;
+  if (search) {
+    newProducts = newProducts.filter((product) => {
+      return product.name.includes(search);
+    });
+  }
+  if (limit) {
+    newProducts = newProducts.slice(0, Number(limit));
+  }
+  if (priceLimit) {
+    newProducts = newProducts.filter((product) => {
+      return Number(product.price) <= priceLimit;
+    });
+  }
+  res.json(newProducts);
+});
+
 app.all("*", (req, res) => {
   res.status(404).send("<h1> Oh no! Your page was not found.</h1>");
 });
