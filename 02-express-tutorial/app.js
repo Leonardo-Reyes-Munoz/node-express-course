26526a39fe33ecb0453f5db577523ad1070922ef
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-10-20 11:49:17 -0700

    update tutorial with use middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,56 +1,22 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+const logger = require("./logger");
 
-app.get("/", (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">Products</a>');
+// req => middleware => res
+app.use(logger);
+
+app.get("/", logger, (req, res) => {
+  res.send("Home");
 });
 
-// app.get("/api/products", (req, res) => {
-//   const newProducts = products.map((product) => {
-//     const { id, name, image } = product;
-//     return { id, name, image };
-//   });
-//   res.json(newProducts);
-// });
-
-app.get("/api/products/:productID", (req, res) => {
-  // console.log(req);
-  console.log(req.params);
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send(`Product "${productID}" Does not Exist`);
-  }
-  res.json(singleProduct);
+app.get("/about", (req, res) => {
+  res.send("About");
 });
 
-app.get("/api/products/:productsID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("Hello test");
-});
-
-app.get("/api/v1/products", (req, res) => {
-  console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({ success: true, data: [] });
-  }
-  res.status(200).json(sortedProducts);
+app.all("*", (req, res) => {
+  res.status(404).send("Page not found");
 });
 
 app.listen(3000, () => {
-  console.log("Server is listening on PORT 3000! What what!");
+  console.log("Server is listening on port 3000 :-)");
 });
